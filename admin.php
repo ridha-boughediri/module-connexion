bbd608679d27d1f07fa3349500def655d7d6b082
ridha-boughediri <ridh.bouhjediri@laplateforme.io>
2021-11-22 11:42:42 +0100

    modificationpourplesk

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$bdd = new PDO('mysql:host=localhost;dbname=moduleconnexion', 'root', '');
+$bdd = new PDO('mysql:host=localhost:3306 ;dbname=ridha-boughediri_moduleconnexion', 'ridha-boughediri', '1234fivem@@@');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $membres = $bdd->query("SELECT * FROM utilisateurs");
 
