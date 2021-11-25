d33a33fd897418971122609e5bf4e642e5e2a228
ridha-boughediri <ridh.bouhjediri@laplateforme.io>
2021-11-25 12:03:59 +0100

    modifppourplesk

--- fileconfig/startbdd.php
+++ fileconfig/startbdd.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$bdd = new PDO('mysql:host=localhost;dbname=moduleconnexion', 'root', '');
+$bdd = new PDO('mysql:host=localhost:3306 ;dbname=ridha-boughediri_moduleconnexion', 'ridha-boughediri', '1234fivem@@@');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
